9f0cfc7751d7dc8f6c5fc30896cb9915346eabcb
Dvir Meir Perkin <73073753+Dvirperkin@users.noreply.github.com>
2022-08-04 12:11:06 +0300

    Documentation, Redundant code deleted
    All the information of the Resources has been printed to file.
    Redundant code has been deleted.

--- main.py
+++ main.py
@@ -45,12 +45,19 @@ def kernels_creator(image, start_y, end_y, start_x, end_x, threshold):
 
 
 def display_figures(original_image, filtered_red_lights, filtered_green_lights):
-    # Displays original image and the convolutions images.
+    """
+    Displays original image and the convolutions images with green and red dots
+    that indicates what the program founded has traffic lights.
+    :param original_image: The original image.
+    :param filtered_red_lights: The x,y coordinates for the detected optional traffic lights with red bulb.
+    :param filtered_green_lights: The x,y coordinates for the detected optional traffic lights with green bulb.
+    :return: None
+    """
 
     plt.imshow(original_image)
-    if (len(filtered_green_lights) != 0):
+    if len(filtered_green_lights) != 0:
         plt.plot(filtered_green_lights[:, 1], filtered_green_lights[:, 0], 'g.')
-    if (len(filtered_red_lights) != 0):
+    if len(filtered_red_lights) != 0:
         plt.plot(filtered_red_lights[:, 1], filtered_red_lights[:, 0], 'r.')
     plt.autoscale(False)
     plt.axis('off')
@@ -77,7 +84,7 @@ def find_tfl_lights(c_image: np.ndarray, **kwargs):
     elif not len(green_tfl):
         tfl_with_info = green_with_info
     else:
-        np.concatenate([red_with_info, green_with_info])
+        tfl_with_info = np.concatenate([red_with_info, green_with_info])
 
     current_data_frame = pd.DataFrame(tfl_with_info, columns=["Image", "y-coordinate", "x-coordinate",
                                                               "light", "RGB", "pixel_light"])
@@ -85,7 +92,7 @@ def find_tfl_lights(c_image: np.ndarray, **kwargs):
     db = DataBase()
     db.add(current_data_frame)
 
-    display_figures(c_image, red_tfl, green_tfl)
+    # display_figures(c_image, red_tfl, green_tfl)
 
     if not len(red_tfl):
         return [], [], green_tfl[:, 0], green_tfl[:, 1]
@@ -111,7 +118,7 @@ def find_light_coordinates(image: np.array, kernel: Kernel, dimension: int, thre
     # Performs the convolution process on the red dimension and the green dimension of the image separately.
     convolution_image_red = kernel.convolution(image[:, :, dimension].copy())
 
-    tfl = np.argwhere(maximum_filter(convolution_image_red, 3) > threshold)
+    tfl = np.argwhere(maximum_filter(convolution_image_red, 1) > threshold)
 
     filtered_tfl = []
 
@@ -135,39 +142,6 @@ def find_light_coordinates(image: np.array, kernel: Kernel, dimension: int, thre
     return tfl_with_info, np.array(filtered_tfl)
 
 
-### GIVEN CODE TO TEST YOUR IMPLENTATION AND PLOT THE PICTURES
-def show_image_and_gt(image, objs, fig_num=None):
-    plt.figure(fig_num).clf()
-    plt.imshow(image)
-    labels = set()
-    if objs is not None:
-        for o in objs:
-            poly = np.array(o['polygon'])[list(np.arange(len(o['polygon']))) + [0]]
-            plt.plot(poly[:, 0], poly[:, 1], 'r', label=o['label'])
-            labels.add(o['label'])
-        if len(labels) > 1:
-            plt.legend()
-
-
-def test_find_tfl_lights(image_path, json_path=None, fig_num=None):
-    """
-    Run the attention code
-    """
-    image = np.array(Image.open(image_path))
-    if json_path is None:
-        objects = None
-    else:
-        gt_data = json.load(open(json_path))
-        what = ['traffic light']
-        objects = [o for o in gt_data['objects'] if o['label'] in what]
-
-    show_image_and_gt(image, objects, fig_num)
-
-    red_x, red_y, green_x, green_y = find_tfl_lights(image)
-    plt.plot(red_x, red_y, 'ro', color='r', markersize=4)
-    plt.plot(green_x, green_y, 'ro', color='g', markersize=4)
-
-
 def main(argv=None):
     """It's nice to have a standalone tester for the algorithm.
     Consider looping over some images from here, so you can manually exmine the results
@@ -183,57 +157,17 @@ def main(argv=None):
                                          end_x=1133,
                                          threshold=232)
 
-    paths = ['Test/berlin_000540_000019_leftImg8bit.png',
-             'Test/berlin_000522_000019_leftImg8bit.png',
-             'Test/berlin_000455_000019_leftImg8bit.png',
-             'Test/bremen_000145_000019_leftImg8bit.png',
-             'Test/darmstadt_000053_000019_leftImg8bit.png',
-             'Test/jena_000032_000019_leftImg8bit.png',
-             'Test/stuttgart_000004_000019_leftImg8bit.png',
-             'Test/ulm_000052_000019_leftImg8bit.png',
-             'Test/bremen_000004_000019_leftImg8bit.png',
-             'Test/darmstadt_000034_000019_leftImg8bit.png',
-             'Test/dusseldorf_000143_000019_leftImg8bit.png',
-             'Test/krefeld_000000_036299_leftImg8bit.png',
-             'Test/stuttgart_000175_000019_leftImg8bit.png',
-             'Test/zurich_000080_000019_leftImg8bit.png',
-             'Test/berlin_000526_000019_leftImg8bit.png',
-             'Test/bremen_000084_000019_leftImg8bit.png',
-             'Test/darmstadt_000043_000019_leftImg8bit.png',
-             'Test/hamburg_000000_067799_leftImg8bit.png',
-             'Test/tubingen_000120_000019_leftImg8bit.png',
-             ]
+    # Opens each PNG and start the convolution process.
+    for root, dirs, files in os.walk('./Resource/leftImg8bit/train'):
+        for file in files:
+            path = root + '/' + file
+            original_image = np.array(Image.open(path))
 
-    for path in paths:
-        original_image = np.array(Image.open(path))
+            find_tfl_lights(original_image, path=path, kernel_red_light=kernel_red_light,
+                            kernel_green_light=kernel_green_light)
 
-        find_tfl_lights(original_image, path=path, kernel_red_light=kernel_red_light,
-                        kernel_green_light=kernel_green_light)
-
-    plt.show(block=True)
-
-    # parser = argparse.ArgumentParser("Test TFL attention mechanism")
-    # parser.add_argument('-i', '--image', type=str, help='Path to an image')
-    # parser.add_argument("-j", "--json", type=str, help="Path to json GT for comparison")
-    # parser.add_argument('-d', '--dir', type=str, help='Directory to scan images in')
-    # args = parser.parse_args(argv)
-    # default_base = "INSERT_YOUR_DIR_WITH_PNG_AND_JSON_HERE"
-    #
-    # if args.dir is None:
-    #     args.dir = default_base
-    # flist = glob.glob(os.path.join(args.dir, '*_leftImg8bit.png'))
-    #
-    # for image in flist:
-    #     json_fn = image.replace('_leftImg8bit.png', '_gtFine_polygons.json')
-    #
-    #     if not os.path.exists(json_fn):
-    #         json_fn = None
-    #     test_find_tfl_lights(image, json_fn)
-    #
-    # if len(flist):
-    #     print("You should now see some images, with the ground truth marked on them. Close all to quit.")
-    # else:
-    #     print("Bad configuration?? Didn't find any picture to show")
+    db = DataBase()
+    db.print_to_file()
 
 
 if __name__ == '__main__':
